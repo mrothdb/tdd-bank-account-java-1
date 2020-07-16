e3f47d7b7fb95019d96344791597d0a7a6350526
mrothdb <matthias.roth@bigwebserver.de>
2020-07-16 16:24:28 +0200

    First commit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,58 @@
 package org.xpdojo.bank;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Account {
+    private String acctNumber;
+    private double balance;
+    private Date businessDate;
+    private String transactionActivity = null;
+
+    public Account(String acctNum, double initialBalance)
+    {
+        acctNumber = acctNum;
+        balance = initialBalance;
+        transactionActivity = null;
+        businessDate = new Date();
+    }
+
+    public void depositAmount( double moneyToBeDeposited) {
+        this.balance= this.balance + moneyToBeDeposited;
+        this.transactionActivity= "Deposit";
+    }
+
+    public void withdrawAmount( double moneyToBeWithdrawn) {
+        this.balance= this.balance - moneyToBeWithdrawn;
+        this.transactionActivity="Withdraw";
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void printReport ()
+    {
+        System.out.println("As of Business Date:" +  businessDate);
+        System.out.println("Account Number:" + this.acctNumber);
+        System.out.println("Transaction Activity:" +  this.transactionActivity);
+        System.out.println("Balance:" +  this.balance);
+
+
+    }
+    public void printBalanceReport ()
+    {
+        System.out.println("As of Business Date:" +  businessDate);
+        System.out.println("Account Number:" + this.acctNumber);
+        System.out.println("Balance:" +  this.balance);
+
+
+    }
+    public static void transferFunds(Account fromAccount, Account toAccount,  double transferAmount)
+    {
+        if (fromAccount.getBalance() >= transferAmount)
+        { fromAccount.withdrawAmount(transferAmount);
+            toAccount.depositAmount(transferAmount);
+        }
+    }
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,13 +3,83 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Assertions;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+    @Disabled
+    // @Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
+        // assertThat("your first test").isBlank();
+
+        Account testAccount = new Account("22222", 100);
+        //assertThat("your GEE first test isn't implemented", true, is(false));
+        testAccount.depositAmount(50);
+        Assertions.assertTrue(testAccount.getBalance()==150.0);
+
+        testAccount.printReport();
+        //assertThat("your first test").isBlank();
+        System.out.println("Your first test case goes here");
     }
+    public void depositMultipleAmountToIncreaseTheBalance() {
+        // assertThat("your first test").isBlank();
+
+        Account testAccount = new Account("11111", 100);
+        //assertThat("your GEE first test isn't implemented", true, is(false));
+        testAccount.depositAmount(50);
+        testAccount.depositAmount(70);
+        Assertions.assertTrue(testAccount.getBalance()==120.0);
+
+        testAccount.printReport();
+        //assertThat("your first test").isBlank();
+        System.out.println("Your first test case goes here");
+    }
+    @Test
+    //@Disabled
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account testAccount = new Account("11111", 100);
+        //assertThat("your GEE first test isn't implemented", true, is(false));
+        testAccount.withdrawAmount(50);
+        Assertions.assertTrue(testAccount.getBalance()==50.0);
+        testAccount.printReport();
+    }
+    @Test
+    //@Disabled
+    public  void transferAnAmountBetweenAccounts() {
+        Account fromAccount = new Account("11111", 100);
+        Account toAccount = new Account("222222", 150);
+        //assertThat("your GEE first test isn't implemented", true, is(false));
+        Account.transferFunds(fromAccount, toAccount, 50.0 ) ;
+        Assertions.assertTrue(fromAccount.getBalance()==50.0 && toAccount.getBalance()==200.0);
+
+    }
+    @Test
+    public  void transferAnAmountInSufficientFunds() {
+        Account fromAccount = new Account("11111", 100);
+        Account toAccount = new Account("222222", 150);
+        //assertThat("your GEE first test isn't implemented", true, is(false));
+        Account.transferFunds(fromAccount, toAccount, 250.0 ) ;
+        Assertions.assertTrue(fromAccount.getBalance()==100.0 && toAccount.getBalance()==150.0);
+
+    }
+
+    @Test
+    public  void printAcitivity() {
+        Account testAccount = new Account("11111", 100);
+        testAccount.printReport();
+        Assertions.assertTrue(testAccount.getBalance()==100.0 );
+    }
+    @Test
+    public  void printBalanceReport() {
+        Account testAccount = new Account("11111", 100);
+        testAccount.printBalanceReport();
+        Assertions.assertTrue(testAccount.getBalance()==100.0 );
+    }
+
+
+
+
 }
